3dc91ed4e16261c50eac7ac7359a4479e5f1d9b5
yuokawara <solbonta1@gmail.com>
2019-03-27 00:46:47 +0900

    kadai201906

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //追加//
+    public function add()
+    {return view('admin.profile.create');}
+    public function create()
+    {return redirect('admin/profile/create');}
+    public function edit()
+    {return view('admin.profile.edit');}
+    public function update()
+    {return redirect('admin/profile/edit');}
+}
